53641775c586284038aad072c9697e78a730ad18
Michael Kasprzyk <michal0kasprzyk@gmail.com>
2019-05-05 16:16:57 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+    name='parliament-agents',
+    version='0.0.1',
+    packages=['parliament-agents',],
+    description='Framework to simulate parliaments behaviour using SPADE',
+    author='M. Budziaszek, T. Michalec, M. Kasprzyk',
+    author_email='budziaszek.m@gmail.com, tomasz.michalec1996@gmail.com, michal0kasprzyk@gmail.com',
+    url='https://github.com/KasprzykM/parliament-agents',
+
+)
